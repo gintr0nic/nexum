a3e1375e2147b6e499a893e6a575400a40c36c54
mrgian <mail@gian.im>
2022-09-25 19:01:54 +0200

    Add migrations

---
+++ database/migrations/create_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('username')->unique();
+            $table->string('name');
+            $table->string('surname');
+            $table->enum('sex', array('male','female'));
+            $table->date('birthdate');
+            $table->enum('role', array('user','moderator', 'admin'))->default('user');
+            $table->boolean('private')->default(false);
+            $table->string('friends')->default('');
+            $table->string('bio')->default('');
+            $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
