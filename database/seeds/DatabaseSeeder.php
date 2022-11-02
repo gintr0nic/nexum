7a76cf1f4fb9ccd6d08e7f7b869d2d2ffe513b2a
mrgian <mail@gian.im>
2022-11-02 18:58:48 +0100

    Update seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,7 +12,7 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         DB::table('users')->insert([
-            ['id' => 1, 'username' => 'gian', 
+            ['id' => 1, 'username' => 'blogblog', 
             'name' => 'Gianmatteo', 
             'surname' => 'Palmieri',
             'sex' => 'male',
@@ -23,20 +23,20 @@ class DatabaseSeeder extends Seeder
             'private' => true, 
             'friends' => '', 
             'bio' => 'Ciao sono Gianmatteo. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
 
             ['id' => 2, 'username' => 'giulia', 
             'name' => 'Giulia', 
             'surname' => 'Rossi',
             'sex' => 'female',
             'address' => 'Via dei Marsi 59',
-            'city' => 'San Nicandro Garganico',
-            'birthdate' => '2000-03-06',
+            'city' => 'Ancona',
+            'birthdate' => '1999-02-11',
             'role' => 'user', 
             'private' => true, 
             'friends' => '', 
             'bio' => 'Ciao sono Giulia. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
 
             ['id' => 3, 'username' => 'mario', 
             'name' => 'Mario', 
@@ -49,10 +49,10 @@ class DatabaseSeeder extends Seeder
             'private' => true, 
             'friends' => '', 
             'bio' => 'Ciao sono Mario. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
 
-            ['id' => 4, 'username' => 'alex', 
-            'name' => 'Alex', 
+            ['id' => 4, 'username' => 'luca', 
+            'name' => 'Luca', 
             'surname' => 'Rossi',
             'sex' => 'male',
             'address' => 'Via dei Marsi 59',
@@ -61,24 +61,11 @@ class DatabaseSeeder extends Seeder
             'role' => 'user', 
             'private' => true, 
             'friends' => '', 
-            'bio' => 'Ciao sono Alex. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
+            'bio' => 'Ciao sono Luca. Questa è la mia biografia.', 
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
 
-            ['id' => 5, 'username' => 'admin', 
-            'name' => 'Alex', 
-            'surname' => 'Rossi',
-            'sex' => 'male',
-            'address' => 'Via dei Marsi 59',
-            'city' => 'San Nicandro Garganico',
-            'birthdate' => '2000-03-06',
-            'role' => 'admin', 
-            'private' => true, 
-            'friends' => '', 
-            'bio' => 'Ciao sono Admin. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
-
-            ['id' => 6, 'username' => 'staff', 
-            'name' => 'Alex', 
+            ['id' => 5, 'username' => 'staffstaff', 
+            'name' => 'Giorgio', 
             'surname' => 'Rossi',
             'sex' => 'male',
             'address' => 'Via dei Marsi 59',
@@ -87,20 +74,33 @@ class DatabaseSeeder extends Seeder
             'role' => 'staff', 
             'private' => true, 
             'friends' => '', 
-            'bio' => 'Ciao sono Admin. Questa è la mia biografia.', 
-            'password' => '$2y$10$fNsWl0GTIUolV68.5BSIr.6K1NFw5Nm/0nf8stRwAN8pJKCbIojdC'],
+            'bio' => 'Ciao sono Giorgio. Questa è la mia biografia.', 
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
+
+            ['id' => 6, 'username' => 'adminadmin', 
+            'name' => 'Nicola', 
+            'surname' => 'Rossi',
+            'sex' => 'male',
+            'address' => 'Via dei Marsi 59',
+            'city' => 'San Nicandro Garganico',
+            'birthdate' => '2000-03-06',
+            'role' => 'admin', 
+            'private' => true, 
+            'friends' => '', 
+            'bio' => 'Ciao sono Nicola. Questa è la mia biografia.', 
+            'password' => '$2y$10$w1f8NBUpq1fYTNb2M6PSNOUGqK3mCVsqy54OB7RbcgEPA8zZNjiSG'],
         ]);
 
         DB::table('blogs')->insert([
             ['id' => 1, 'blogname' => 'tennis', 
             'name' => 'Blog di Tennis',
             'topic' => 'In questo blog si parla di tennis',
-            'owner' => 'gian'],
+            'owner' => 'blogblog'],
 
             ['id' => 2, 'blogname' => 'politica', 
             'name' => 'Blog di politica',
             'topic' => 'In questo blog si parla di politica',
-            'owner' => 'gian'],
+            'owner' => 'blogblog'],
 
             ['id' => 3, 'blogname' => 'moda', 
             'name' => 'Blog di moda',
@@ -109,11 +109,11 @@ class DatabaseSeeder extends Seeder
         ]);
 
         DB::table('posts')->insert([
-            ['id' => 1, 'author' => 'gian', 
+            ['id' => 1, 'author' => 'blogblog', 
             'blog' => 'tennis', 
             'text' => 'Ciao a tutti! Questo è il mio primo post sul mio nuovo blog che parla di tennis'],
 
-            ['id' => 2, 'author' => 'gian', 
+            ['id' => 2, 'author' => 'blogblog', 
             'blog' => 'politica', 
             'text' => 'Ciao a tutti! Questo è il mio primo post sul mio nuovo blog che parla di politica'],
 
@@ -125,21 +125,21 @@ class DatabaseSeeder extends Seeder
         DB::table('friend_requests')->insert([
             ['id' => 1,
             'from' => 'giulia', 
-            'to' => 'gian'],
+            'to' => 'blogblog'],
 
             ['id' => 2,
             'from' => 'mario', 
-            'to' => 'gian'],
+            'to' => 'blogblog'],
 
             ['id' => 3,
-            'from' => 'alex', 
-            'to' => 'gian'],
+            'from' => 'luca', 
+            'to' => 'blogblog'],
         ]);
 
         DB::table('messages')->insert([
             ['id' => 1,
             'from' => 'admin', 
-            'to' => 'gian',
+            'to' => 'blogblog',
             'text' => 'Messaggio di prova'],
 
             ['id' => 2,
@@ -149,12 +149,12 @@ class DatabaseSeeder extends Seeder
 
             ['id' => 3,
             'from' => 'admin', 
-            'to' => 'gian',
+            'to' => 'blogblog',
             'text' => 'Messaggio di prova 2'],
 
             ['id' => 4,
             'from' => 'admin', 
-            'to' => 'gian',
+            'to' => 'blogblog',
             'text' => 'Messaggio di prova 3'],
         ]); 
     }
